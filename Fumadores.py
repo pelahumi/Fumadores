b3d9f389a7d9883b99502eb52f90f6a6d3eb6cf1
Pelayo <phuermij@myuax.com>
2023-05-15 17:14:36 +0200

    Clase fumador

--- Fumadores.py
+++ Fumadores.py
@@ -5,6 +5,22 @@ import time
 #Creamos una lista con los ingredientes
 INGREDIENTES = ["papel", "tabaco", "filtros", "green", "cerillas"]
 
-# Condiciones
+#Condiciones
 puede_fumar = threading.Condition()
 disponible = threading.Condition()
+
+#Clase fumador
+class Fumador(threading.Thread):
+    def __init__(self, ingrediente):
+        super().__init__(self)
+        self.ingrediente = ingrediente
+    
+    def run(self):
+        while True:
+            with puede_fumar:
+                puede_fumar.wait()
+                if self.ingrediente in INGREDIENTES:
+                    print("Fumador con ",self.ingrediente, "cogió el resto y está fumando.")
+                    time.sleep(2.5)
+                    puede_fumar.notify()
+            
